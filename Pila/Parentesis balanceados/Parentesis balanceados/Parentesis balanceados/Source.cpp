de95a66029efc4039e49195487bc7a6e7e6b6519
David de los Santos Gil <32543989+daviddelossan@users.noreply.github.com>
2017-10-31 00:18:23 +0100

    Add files via upload

---
+++ Pila/Parentesis balanceados/Parentesis balanceados/Parentesis balanceados/Source.cpp
@@ -0,0 +1,207 @@
+﻿//
+//  stack_eda.h
+//
+//  Implementación del TAD pila con array dinámico
+//
+//  Estructuras de Datos y Algoritmos
+//  Facultad de Informática
+//  Universidad Complutense de Madrid
+//
+//  Created by Alberto Verdejo on 6/12/16.
+//  Copyright (c) 2016 Alberto Verdejo. All rights reserved.
+//
+
+#ifndef stack_eda_h
+#define stack_eda_h
+
+#include <stdexcept>
+
+template <class T>
+class stack {
+	static const int TAM_INICIAL = 10; // tamaño inicial del array dinámico
+public:
+
+	// constructor: pila vacía
+	stack() : nelems(0), capacidad(TAM_INICIAL), array(new T[capacidad]) {}
+
+	// destructor
+	~stack() {
+		libera();
+	}
+
+	// constructor por copia
+	stack(stack<T> const& other) {
+		copia(other);
+	}
+
+	// operador de asignación
+	stack<T> & operator=(stack<T> const& other) {
+		if (this != &other) {
+			libera();
+			copia(other);
+		}
+		return *this;
+	}
+
+	// apilar un elemento
+	void push(T const& elem) {
+		if (nelems == capacidad)
+			amplia();
+		array[nelems] = elem;
+		++nelems;
+	}
+
+	// consultar la cima
+	T const& top() const {
+		if (empty())
+			throw std::domain_error("la pila vacia no tiene cima");
+		return array[nelems - 1];
+	}
+
+	// desapilar el elemento en la cima
+	void pop() {
+		if (empty())
+			throw std::domain_error("desapilando de la pila vacia");
+		--nelems;
+	}
+
+	// consultar si la pila está vacía
+	bool empty() const {
+		return nelems == 0;
+	}
+
+protected:
+
+	// número de elementos en la pila
+	size_t nelems;
+
+	// tamaño del array
+	size_t capacidad;
+
+	// puntero al array que contiene los datos
+	T * array;
+
+	void libera() {
+		delete[] array;
+		array = nullptr;
+	}
+
+	void copia(stack const& other) {
+		capacidad = other.nelems;
+		nelems = other.nelems;
+		array = new T[capacidad];
+		for (size_t i = 0; i < nelems; ++i)
+			array[i] = other.array[i];
+	}
+
+	void amplia() {
+		T * viejo = array;
+		capacidad *= 2;
+		array = new T[capacidad];
+
+		for (size_t i = 0; i < nelems; ++i)
+			array[i] = std::move(viejo[i]);
+
+		delete[] viejo;
+	}
+};
+
+#endif // stack_eda_h
+
+
+
+
+
+
+
+
+// Grupo E67, David de los Santos Gil
+// Comentario general sobre la solución,
+// explicando cómo se resuelve el problema
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <assert.h>
+#include <stdio.h>
+#include <algorithm>
+#include <string>
+
+
+
+
+// El método va metiendo en la pila cada paréntesis abierto, y los va sacando según encuentra su correspondiente paréntesis cerrado
+// Si en algún momento no coincide o al final la pila no está vacía, devolvemos false
+// El coste del algoritmo es lineal en el número de caracteres de la linea
+bool balanceado(std::string linea) {
+
+	stack<char> pila;
+	size_t tam = linea.size();
+
+
+	for (int i = 0; i < tam; ++i){
+		if (linea[i] == '(' || linea[i] == '{' || linea[i] == '['){
+			pila.push(linea[i]);
+		}
+		else if (linea[i] == ')'){
+			if (pila.empty() || pila.top() != '(') return false;
+			else pila.pop();
+		}
+		else if (linea[i] == '}'){
+			if (pila.empty() || pila.top() != '{') return false;
+			else pila.pop();
+		}
+		else if (linea[i] == ']'){
+			if (pila.empty() || pila.top() != '[') return false;
+			else pila.pop();
+		}
+	}
+
+	if (pila.empty()) return true;
+	else return false;
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	std::string linea;
+	char c = 'a';
+
+	getline(std::cin, linea);
+
+	if (!std::cin)
+		return false;
+
+	if (balanceado(linea)) std::cout << "YES" << std::endl;
+	else std::cout << "NO" << std::endl;
+
+	// escribir sol
+
+
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+	/*
+#ifndef DOMJUDGE
+	std::ifstream in("Casos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+	*/
+
+	while (resuelveCaso())
+		;
+
+	/*
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	//system("PAUSE");
+#endif*/
+
+	return 0;
+}
---
+++ Pila/Solitario/Solitario/Source.cpp
@@ -0,0 +1,195 @@
+﻿
+// Grupo E67, David de los Santos Gil
+// Comentario general sobre la solución,
+// explicando cómo se resuelve el problema
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <assert.h>
+#include <stdio.h>
+#include <algorithm>
+#include <string>
+#include "stack_eda.h"
+
+
+
+typedef struct tCarta{
+	int palo;
+	int numero;
+};
+
+
+// El método va iterando hasta que ya no queden cartas o hayamos dado una vuelta entera sin poner una carta
+bool resolver(stack<tCarta> cartasMano) {
+	stack<tCarta> cartasDescubiertas;
+	tCarta carta;
+	stack<tCarta> cartasOros;
+	stack<tCarta> cartasCopas;
+	stack<tCarta> cartasEspadas;
+	stack<tCarta> cartasBastos;
+	bool puedePoner, vueltaPoniendo = true, resultado = false;
+
+
+	while(vueltaPoniendo && !cartasMano.empty()){
+		vueltaPoniendo = false;
+
+		while (!cartasMano.empty()) { //Mientras que siga habiendo cartas cubiertas
+			carta = cartasMano.top();
+			cartasDescubiertas.push(carta);
+			cartasMano.pop();
+
+			if (!cartasMano.empty()) {
+				carta = cartasMano.top();
+				cartasDescubiertas.push(carta);
+				cartasMano.pop();
+			}
+
+			puedePoner = true;
+
+			while (puedePoner) {
+				if (!cartasDescubiertas.empty() && cartasDescubiertas.top().palo == 1) { //Si hay cartas descubiertas y el palo son oros
+					if (cartasDescubiertas.top().numero == 1 || // Y la carta es el as, o bien es la siguiente que encaja en la pila de oros
+						(!cartasOros.empty() && cartasDescubiertas.top().numero == cartasOros.top().numero + 1)) {
+						cartasOros.push(cartasDescubiertas.top());
+						cartasDescubiertas.pop();
+						vueltaPoniendo = true;
+					}
+					else puedePoner = false;
+				}
+
+				else if (!cartasDescubiertas.empty() && cartasDescubiertas.top().palo == 2) { //Si el palo son copas
+					if (cartasDescubiertas.top().numero == 1 ||
+						(!cartasCopas.empty() && cartasDescubiertas.top().numero == cartasCopas.top().numero + 1)) {
+						cartasCopas.push(cartasDescubiertas.top());
+						cartasDescubiertas.pop();
+						vueltaPoniendo = true;
+					}
+					else puedePoner = false;
+				}
+
+				else if (!cartasDescubiertas.empty() && cartasDescubiertas.top().palo == 3) { //Si el palo son espadas
+					if (cartasDescubiertas.top().numero == 1 ||
+						(!cartasEspadas.empty() && cartasDescubiertas.top().numero == cartasEspadas.top().numero + 1)) {
+						cartasEspadas.push(cartasDescubiertas.top());
+						cartasDescubiertas.pop();
+						vueltaPoniendo = true;
+					}
+					else puedePoner = false;
+				}
+
+				else if (!cartasDescubiertas.empty() && cartasDescubiertas.top().palo == 4) { //Si el palo son bastos
+					if (cartasDescubiertas.top().numero == 1 ||
+						(!cartasBastos.empty() && cartasDescubiertas.top().numero == cartasBastos.top().numero + 1)) {
+						cartasBastos.push(cartasDescubiertas.top());
+						cartasDescubiertas.pop();
+						vueltaPoniendo = true;
+					}
+					else puedePoner = false;
+				}
+				else puedePoner = false;
+			}
+
+		}
+
+		//Ponemos las descubiertas en el mazo de la mano
+
+		while (!cartasDescubiertas.empty()) {
+			cartasMano.push(cartasDescubiertas.top());
+			cartasDescubiertas.pop();
+		}
+	}
+
+
+	if (cartasMano.empty()) {
+		if (!cartasOros.empty())
+			if (cartasOros.top().numero != 10)
+				return false;
+		if (!cartasCopas.empty())
+			if (cartasCopas.top().numero != 10)
+				return false;
+		if (!cartasEspadas.empty())
+			if (cartasEspadas.top().numero != 10)
+				return false;
+		if (!cartasBastos.empty())
+			if (cartasBastos.top().numero != 10)
+				return false;
+	}
+	else return false;
+
+	return true;
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	char c;
+	int palos;
+	tCarta carta;
+	stack<tCarta> cartasMano;
+	stack<tCarta> cartasOrdenadas;
+
+
+	std::cin >> palos;
+
+	if (palos == 0)
+		return false;
+
+	else {
+
+		for (int i = 0; i < palos * 10; ++i) {
+			std::cin >> carta.numero;
+			std::cin >> c;
+
+			if (carta.numero == 10 || carta.numero == 11 || carta.numero == 12) carta.numero = carta.numero - 2;
+
+			if (c == 'O') carta.palo = 1;
+			else if (c == 'C') carta.palo = 2;
+			else if (c == 'E') carta.palo = 3;
+			else carta.palo = 4;
+
+			cartasMano.push(carta);
+		}
+	}
+	
+	while (!cartasMano.empty()) {
+		cartasOrdenadas.push(cartasMano.top());
+		cartasMano.pop();
+	}
+	
+
+	if (resolver(cartasOrdenadas)) std::cout << "GANA" << std::endl;
+	else std::cout << "PIERDE" << std::endl;
+
+	// escribir sol
+
+
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+	
+	#ifndef DOMJUDGE
+	std::ifstream in("Casos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+	#endif
+	
+
+	while (resuelveCaso())
+		;
+
+	
+	// Para restablecer entrada. Comentar para acepta el reto
+	#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	//system("PAUSE");
+
+	
+	#endif
+	
+	return 0;
+}
